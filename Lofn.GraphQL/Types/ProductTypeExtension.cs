12c9dc2273573b92538dfc58a6a6861a25db7bd3
Rodrigo Landim <rodrigo@emagine.com.br>
2026-03-23 09:29:19 -0300

    fix: change MaxFieldCost to 8000 on graphQL

--- Lofn.GraphQL/Types/ProductTypeExtension.cs
+++ Lofn.GraphQL/Types/ProductTypeExtension.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using HotChocolate.Types;
 using Lofn.Domain.Interfaces;
 using Lofn.Infra.Context;
+using Microsoft.EntityFrameworkCore;
 using zTools.ACL.Interfaces;
 
 namespace Lofn.GraphQL.Types;
@@ -10,24 +11,26 @@ public class ProductTypeExtension : ObjectTypeExtension<Product>
 {
     protected override void Configure(IObjectTypeDescriptor<Product> descriptor)
     {
-        descriptor.Field(t => t.ProductImages).IsProjected(true);
-
         descriptor
             .Field("imageUrl")
             .Type<StringType>()
             .Resolve(async ctx =>
             {
                 var product = ctx.Parent<Product>();
-                var firstImage = product.ProductImages?
-                    .OrderBy(i => i.SortOrder)
-                    .FirstOrDefault();
+                var dbContext = ctx.Service<LofnContext>();
 
-                if (firstImage == null || string.IsNullOrEmpty(firstImage.Image))
+                var firstImage = await dbContext.ProductImages
+                    .Where(i => i.ProductId == product.ProductId)
+                    .OrderBy(i => i.SortOrder)
+                    .Select(i => i.Image)
+                    .FirstOrDefaultAsync();
+
+                if (string.IsNullOrEmpty(firstImage))
                     return null;
 
                 var fileClient = ctx.Service<IFileClient>();
                 var tenantResolver = ctx.Service<ITenantResolver>();
-                return await fileClient.GetFileUrlAsync(tenantResolver.BucketName, firstImage.Image);
+                return await fileClient.GetFileUrlAsync(tenantResolver.BucketName, firstImage);
             });
     }
 }
